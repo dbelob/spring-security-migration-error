11cfdf5e69e4aed9b91c8409caeef588a40ec8bb
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-12-31 23:04:27 +0300

    Configure

---
+++ src/main/java/acme/example/config/WebConfig.java
@@ -0,0 +1,25 @@
+package acme.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/home");
+//        registry.addViewController("/login").setViewName("loginForm");
+    }
+}
