7aa8fa29e028fcae322ffa253dc29fc9bcefd3ff
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2023-01-01 19:44:12 +0300

    Add form login

--- src/main/java/acme/example/config/SecurityConfig.java
+++ src/main/java/acme/example/config/SecurityConfig.java
@@ -15,6 +15,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .formLogin()
+                .and()
             .httpBasic()
                 .and()
             .authorizeRequests()
