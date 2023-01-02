682b7c5c6da99f9046fdf44e43afc9be01c76b5e
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2023-01-02 18:50:21 +0300

    Migrate to Spring Boot 3

--- src/main/java/acme/example/config/SecurityConfig.java
+++ src/main/java/acme/example/config/SecurityConfig.java
@@ -2,27 +2,32 @@ package acme.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .formLogin()
                 .loginPage("/login")
                 .and()
             .httpBasic()
                 .and()
-            .authorizeRequests()
-                .antMatchers("/login", "/public", "/css/**").permitAll()
-                .anyRequest().authenticated();
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/login", "/public", "/css/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
     }
 
     @Bean
@@ -30,12 +35,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user")
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
                 .password("password")
-                .roles("USER");
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
